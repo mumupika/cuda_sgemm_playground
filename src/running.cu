72cf1ca705d14f5f00d92d0e368d2040c91fc6eb
Mumupika <138651100+mumupika@users.noreply.github.com>
2026-03-17 21:54:33 +0800

    Kernel4 (#8)
    * fix: refactor the random scope.
    add: Shared Memory with tiling 1 dimension.
    fix: use Cublas check instead of using the cpu result.
    
    * Add: kernel5 for bank conflict avoiding.

--- src/running.cu
+++ src/running.cu
@@ -87,10 +87,45 @@ void run_kernel3(
     /// launch the kernel from launcher.
     GpuTimer time{};
     time.start();
-    launch_sgemm_smem(M, N, K, dA, dB, dC, alpha, beta, gridDim, blockDim, tileSize);
+    size_t sharedMemSize = 2 * tileSize * tileSize * sizeof(float);
+    launch_sgemm_smem(M, N, K, dA, dB, dC, alpha, beta, gridDim, blockDim, tileSize, sharedMemSize);
     time.stop();
 
     printf("Kernel 3: GPU executed elapsed: %f ms\n", time.elapsed_millis());
+
+    /// Check the data's correctivity.
+    if (check_result_flag) {
+        printf("==========================================================\n");
+        printf("Check with cpu result enabled. Checking...\n");
+        check_cublas_result(M, N, K, hA, hB, hC, dC, alpha, beta);
+        printf("==========================================================\n");
+    }
+}
+
+void run_kernel4(
+    int const M, int const N, int const K,
+    float *hA, float *hB, float *hC,
+    float *dA, float *dB, float *dC,
+    float &alpha, float &beta,
+    bool const check_result_flag) {
+    prepare_matrix(M, N, K, hA, hB, hC, dA, dB, dC, alpha, beta);
+    if (check_result_flag) {
+        check_data(hA, hB, hC, dA, dB, dC);
+    }
+
+    /// Create blocks and grids to map the datas for calculation.
+    dim3 gridDim(CEIL_DIV(M, 32), CEIL_DIV(N, 32), 1);
+    int const tileSize = 32;
+    dim3 blockDim(tileSize, tileSize);
+
+    /// launch the kernel from launcher.
+    GpuTimer time{};
+    time.start();
+    size_t sharedMemSize = 2 * tileSize * tileSize * sizeof(float);
+    launch_sgemm_smem_opt(M, N, K, dA, dB, dC, alpha, beta, gridDim, blockDim, tileSize, sharedMemSize);
+    time.stop();
+
+    printf("Kernel 4: GPU executed elapsed: %f ms\n", time.elapsed_millis());
 
     /// Check the data's correctivity.
     if (check_result_flag) {
