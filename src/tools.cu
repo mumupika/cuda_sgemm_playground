02cca4340d0f7073e4d90325797e2ccb2d7776cb
mumupika <mumujun12345@sjtu.edu.cn>
2026-03-28 07:21:20 +0000

    fix 3: cudaMemcpy2D.

--- src/tools.cu
+++ src/tools.cu
@@ -60,9 +60,9 @@ void memHtoD(
     float *dA, float *dB, float *dC  // Device Data;
 ) {
     /// Now copy the data from host to GPU.
-    CUDA_CHECK(cudaMemcpy(dA, hA, sizeof(float) * M * K, cudaMemcpyHostToDevice));
-    CUDA_CHECK(cudaMemcpy(dB, hB, sizeof(float) * K * N, cudaMemcpyHostToDevice));
-    CUDA_CHECK(cudaMemcpy(dC, hC, sizeof(float) * M * N, cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy2D(dA, ldA * sizeof(float), hA, K * sizeof(float), K * sizeof(float), M, cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy2D(dB, ldB * sizeof(float), hB, N * sizeof(float), N * sizeof(float), K, cudaMemcpyHostToDevice));
+    CUDA_CHECK(cudaMemcpy2D(dC, ldC * sizeof(float), hC, N * sizeof(float), N * sizeof(float), M, cudaMemcpyHostToDevice));
 }
 
 void get_cpu_result(
